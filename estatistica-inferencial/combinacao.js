dc098c0401c22f33ab46a82cc676293d9909caf7
William Dias <williamdiasvargas@gmail.com>
2016-07-14 13:54:05 -0400

    combinacao,permutacao,fatorial

---
+++ estatistica-inferencial/combinacao.js
@@ -0,0 +1,7 @@
+'use strict';
+var fatorial = require('./fatorial');
+function combinacao(n,p) {
+  return fatorial(n)/fatorial(p)*fatorial((n-p))
+}
+module.exports = combinacao;
+console.log(combinacao(5,2));
