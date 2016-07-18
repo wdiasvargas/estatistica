6c80517ae92db2d57c977a5d3ea1a1699ee2f3ab
William Dias <williamdiasvargas@gmail.com>
2016-07-18 15:07:31 -0400

    alteracao de bernoulli

--- estatistica-inferencial/bernoulli.js
+++ estatistica-inferencial/bernoulli.js
@@ -1,6 +1,5 @@
 'use strict';
 function bernoulli (x,p){
-  var q = 1 - p;
-    return Math.pow(p,x)*Math.pow(q,(1-x))
+    return Math.pow(p,x)*Math.pow((1-p),(1-x))
   }
 module.exports = bernoulli;
